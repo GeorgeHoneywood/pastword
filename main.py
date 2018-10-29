f9b5e39cc7d76fbfe85959a47cf073e1a3da1d25
HoneyFox <gitea@honeyfox.uk>
2018-10-29 00:40:11 +0000

    you can kindof edit entries now. needs more work

--- main.py
+++ main.py
@@ -2,31 +2,60 @@ import sys, os, sqlite3
 
 from PyQt4 import QtCore, QtGui, uic
 from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 
-qtCreatorFile = "pastword.ui"
+qtCreatorFile1 = "pastword.ui"
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile1)
  
-Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
- 
+
+class editEntryDialog(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        uic.loadUi("editEntry.ui", self)
+
+        #self.setupUi(self)
+        #self.loadEntry()
+
+    # def loadEntry(self):
+    #     indexes = mainWindow.loginTable.selectionModel().selectedRows()
+    #     print(indexes)
+
 class mainWindow(QtGui.QMainWindow, Ui_MainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
-        self.actionAdd_entry.triggered.connect(self.addEntry)
-        self.actionRemove_entry.triggered.connect(self.removeEntry)
         self.actionOpen.triggered.connect(self.openFile)
         self.actionSave.triggered.connect(self.saveFile)
+        self.actionAdd_entry.triggered.connect(self.addEntry)
+        self.actionRemove_entry.triggered.connect(self.removeEntry)
+        self.actionEdit_entry.triggered.connect(self.editEntry)
     
     def addEntry(self):
+        #print("dab")
         rowPosition = self.loginTable.rowCount()
         self.loginTable.insertRow(rowPosition)
     
     def removeEntry(self):
         indexes = self.loginTable.selectionModel().selectedRows()
-        for row in sorted(indexes):
-            print(row)
+        for row in sorted(indexes, reverse=True):
+            print(row.row())
             self.loginTable.removeRow(row.row())
+
+    def editEntry(self):
+        self.editPopup = editEntryDialog()
+
+        indexes = self.loginTable.selectionModel().selectedRows()
+        index = indexes[0].row()
+
+        self.editPopup.txtSite.setText(self.loginTable.item(index, 0).text())
+        self.editPopup.txtUsername.setText(self.loginTable.item(index, 1).text())
+        self.editPopup.txtEmail.setText(self.loginTable.item(index, 2).text())
+        self.editPopup.txtPassword.setText(self.loginTable.item(index, 3).text())
+        self.editPopup.txtNotes.setText(self.loginTable.item(index, 4).text())
+
+        self.editPopup.exec_()
 
     def openFile(self):
         dbName = QtGui.QFileDialog.getOpenFileName(self, 'Open database')
@@ -77,8 +106,11 @@ class mainWindow(QtGui.QMainWindow, Ui_MainWindow):
 
         dbConn.close()
 
-if __name__ == "__main__":
+def main():
     app = QtGui.QApplication(sys.argv)
     window = mainWindow()
     window.show()
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
